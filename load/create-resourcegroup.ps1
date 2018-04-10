7ec77173754d9122472df0d2436c9301270e749a
mailfra <frmailhot@gmail.com>
2018-04-10 14:56:58 -0400

    updated create-resource group

--- load/create-resourcegroup.ps1
+++ load/create-resourcegroup.ps1
@@ -1,119 +1,66 @@
-
-
-function create-resourceGroup
-
+function create-resourcegroup
 {
 
-    Param(
+Param(
+    [Parameter(Mandatory=$true)]   
+    [string]$subscription,
 
-    [Parameter(Mandatory=$true)]   
+    [Parameter(Mandatory=$true)]   
+    [string]$projectName,
+    
+    [Parameter(Mandatory=$true)]   
+    [string]$projectOwner,
+    
+    [Parameter(Mandatory=$true)]   
+    [string]$pe,
+    [string]$location="canadaeast"
+)
+   
 
-    [string]$subscription,
-
-    [Parameter(Mandatory=$true)]   
-
-    [string]$projectName,
-
-    [Parameter(Mandatory=$true)]   
-
-    [string]$projectOwner,
-
-    [Parameter(Mandatory=$true)]   
-
-    [string]$pe,
-
-    [string]$location="canadaeast"
-
-    )
-
-    
-
-    #Defines the Resource Group Name 
-
-    $num = 1
-
-    
-
- 
-
-    try {
-
-        $sub = Get-AzureRmSubscription -SubscriptionName $subscription -ErrorAction Stop   
-
-        Set-AzureRmContext -Subscription $sub -ErrorAction Stop
-
-    }
-
-    catch {
-
-        Write-Host "No Such Subscription found" -ForegroundColor Yellow
-
-        exit
-
-    }
-
-    
-
-    $created = $false   
+select-azurermsubscription -SubscriptionID $subscription
 
 
- 
 
-    while (!$created) {
+$rg_name = "$projectName$num-$subscription-rg"
+Write-Output "Creating the ResourceGroup $rg_name" 
 
-        $rg_name = "$projectName$num-$subscription-rg"
+$rg = New-AzureRmResourceGroup -Name $rg_name -Location $location -Tag @{Billto=$pe;ProjectName=$projectName;ProjectOwner=$projectOwner}
+ 
 
-        if (!(Check-ResourceGroup -ResourceGroupName $rg_name -Location $location)) {
+        
+#sets parameters for the Policy
+$param = @{"tagName"="billto";"tagValue"=$pe.ToString()}
+    
+#Geta the Apply Tag Builtin Policy
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/2a0e14a6-b0a6-4fab-991a-187a4f81c498
+#Applies the policy to the resource group
+New-AzureRmPolicyAssignment -Name BilltoApply -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
+        
+#Gets the Enforce Tag Builtin Policy
+Write-Output "Adding the Enforce Tag to $rg_name"  
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62 
+    
+#Applies the policy to the resource group
+Write-Output "Adding the Apply Tag to $rg_name"  
+New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
 
-            Write-Host -ForegroundColor Yellow "The RG does not exist. Creating"
-
-            #creates new resource group 
-
-            $rg = New-AzureRmResourceGroup -Name $rg_name -Location $location -Tag @{Billto=$pe;ProjectName=$projectName;ProjectOwner=$projectOwner}
-
-            $created = $true
-
-        }
-
-        else {
-
-            Write-Host -ForegroundColor Yellow "The RG $rg_name exists"
-
-            $num+=1
-
-        }
-
-    }
-
-    #sets parameters for the Policy
-
-    $param = @{"tagName"="billto";"tagValue"=$pe.ToString()}
-
- 
-
-    #Geta the Apply Tag Builtin Policy
-
-    $policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/2a0e14a6-b0a6-4fab-991a-187a4f81c498
-
-    #Applies the policy to the resource group
-
-    New-AzureRmPolicyAssignment -Name BilltoApply -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
-
-    
-
-    #Gets the Enforce Tag Builtin Policy
-
-    $policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62
-
-
- 
-
-    #Applies the policy to the resource group
-
-    New-AzureRmPolicyAssignment -Name BilltoEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
-
- 
+#sets parameters for the Policy
+$param = @{"tagName"="ProjectName";"tagValue"=$projectName.ToString()}
+    
+#Geta the Apply Tag Builtin Policy
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/2a0e14a6-b0a6-4fab-991a-187a4f81c498
+#Applies the policy to the resource group
+New-AzureRmPolicyAssignment -Name ProjectNameApply -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
+        
+#Gets the Enforce Tag Builtin Policy
+Write-Output "Adding the Enforce Tag to $rg_name"  
+$policy = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/1e30110a-5ceb-460c-a204-c1c3969c6d62 
+    
+#Applies the policy to the resource group
+Write-Output "Adding the Apply Tag to $rg_name"  
+New-AzureRmPolicyAssignment -Name ProjectNameEnforce -Scope $rg.ResourceId -PolicyDefinition $policy -PolicyParameterObject $param
 
 
 }
 
+    
